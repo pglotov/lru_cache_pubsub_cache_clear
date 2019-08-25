06d4576e943321e75f2920e97df901536ff83a95
Petr Glotov <pglotov@github.com>
2019-08-25 15:33:34 -0700

    cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lru-cache-pubsub-cache-clear",
-    version="0.0.7",
+    version="0.0.8",
     author="Petr Glotov",
     author_email="pglotov@yahoo.com",
     description="redis based cache_clear() for lru_cache",
